c660bb0c8e9f6c63e48dfd47669dc78e2f2eb81c
KD <kd.engineer@yahoo.co.in>
2013-09-30 15:32:19 +0530

    added #find_collection for AR + specs

--- lib/orm_adapter/adapters/active_record.rb
+++ lib/orm_adapter/adapters/active_record.rb
@@ -23,6 +23,13 @@ module OrmAdapter
       klass.where(conditions_to_fields(conditions)).order(*order_clause(order)).first
     end
 
+    # @see OrmAdapter::Base#find_collection
+    def find_collection(options = {})
+      conditions, order, limit, offset = extract_conditions!(options)
+      klass.where(conditions_to_fields(conditions)).order(*order_clause(order))
+    end
+
+
     # @see OrmAdapter::Base#find_all
     def find_all(options = {})
       conditions, order, limit, offset = extract_conditions!(options)
